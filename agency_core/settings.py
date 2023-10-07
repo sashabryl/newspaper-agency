c45bc37964456483b9fe3d4ffff9cedbf0eb872b
Olexander Bryl <edlrian814@gmail.com>
2023-10-07 20:23:05 +0300

    Add models Redactor Newspaper Topic

--- agency_core/settings.py
+++ agency_core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "bootstrap5"
+    "bootstrap5",
     "newspaper_agency",
 ]
 
@@ -101,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "newspaper_agency.Redactor"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- newspaper_agency/models.py
+++ newspaper_agency/models.py
@@ -1,3 +1,30 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        ordering = ["-years_of_experience"]
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+    topics = models.ManyToManyField("Topic", related_name="newspapers")
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="newspapers"
+    )
+
+    class Meta:
+        ordering = ["-published_date"]
