7c812b9e8003a8be124fb472595c3de793a937a4
Olexander Bryl <edlrian814@gmail.com>
2023-10-08 17:20:36 +0300

    Create login & logged_out & index temlates & index view

--- newspaper_agency/views.py
+++ newspaper_agency/views.py
@@ -1,6 +1,26 @@
+import datetime
+
+from django.contrib.auth import get_user_model
 from django.http import HttpRequest
 from django.shortcuts import render
 
+from newspaper_agency.models import Topic, Newspaper
+
 
 def index(request: HttpRequest):
-    return render(request, "pages/index.html")
+    newspapers = Newspaper.objects.prefetch_related("publishers")
+    num_topics = Topic.objects.all().count()
+    total_newspapers = newspapers.all().count()
+    num_last_month = newspapers.filter(
+        published_date__gt=(
+                datetime.date.today() - datetime.timedelta(weeks=4)
+        )
+    ).count()
+    num_staff = get_user_model().objects.all().count()
+    context = {
+        "num_topics": num_topics,
+        "num_last_month": num_last_month,
+        "total_newspapers": total_newspapers,
+        "num_staff": num_staff
+    }
+    return render(request, "pages/index.html", context=context)
