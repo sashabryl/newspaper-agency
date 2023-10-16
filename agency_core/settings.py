99337e6f9787734e034466ff584eb0bf240477c6
Olexander Bryl <edlrian814@gmail.com>
2023-10-16 17:42:04 +0300

    Add render.com's host to ALLOWED_HOSTS

--- agency_core/settings.py
+++ agency_core/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ]
+ALLOWED_HOSTS = [
+    "localhost", "127.0.0.1", "https://newspaper-agency-9sqf.onrender.com"
+]
 
 # Application definition
 
