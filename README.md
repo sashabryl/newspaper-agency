7b3b77e0530e4efbec0ca5cf4a2bfe0c0f3b88c2
Olexander Bryl <edlrian814@gmail.com>
2023-10-17 21:08:17 +0300

    Place all code on the develop branch

---
+++ README.md
@@ -0,0 +1,24 @@
+# Newspaper Agency
+An app for tracking and managing the publications 
+of a newspaper agency and its staff.
+
+## Check it out!
+[Newspaper agency deployed to render](https://newspaper-agency-9sqf.onrender.com)
+
+## Installation
+```shell
+git clone https://github.com/sashabryl/newspaper-agency.git
+cd news-agency
+python -m venv venv
+source venv/Scripts/activate.bat   #venv/bin/activate on MacOs and Linux
+pip install -r requirements.txt
+python manage.py runserver
+```
+## Features
+* Authentication for staff of the agency as Redactors
+* Keeping track of all issues 
+* Convenient management of the archive
+
+### Credentials for logging in:
+* username: prokop
+* password: asdf!qwe
---
+++ newspaper_agency/models.py
@@ -0,0 +1,39 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["-years_of_experience"]
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+    topics = models.ManyToManyField("Topic", related_name="newspapers")
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="newspapers"
+    )
+
+    class Meta:
+        ordering = ["-published_date"]
+
+    def __str__(self) -> str:
+        return self.title
